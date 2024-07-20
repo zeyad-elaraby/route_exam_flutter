06e93f4402a492a2bc8498e663d67f542b1759c9
zeyad <zeyadelaraby1@gmail.com>
2024-07-20 09:17:21 +0300

    init

---
+++ lib/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = "HomeScreen";
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
